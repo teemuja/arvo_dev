2d33bf92b6a4b9d496de4afc5922284906d7836c
teemuja <teemu.jama@aalto.fi>
2024-03-18 17:01:56 +0000

    v1

--- app/app.py
+++ app/app.py
@@ -1,5 +1,80 @@
 import streamlit as st
+import pandas as pd
+import geopandas as gpd
+from shapely import wkt
+from streamlit_gsheets import GSheetsConnection
+import utils
 
-st.header("Research title")
-st.markdown("Authors: NN,NN.. @[basecamp](##)")
+st.header("A R V O dev",divider='green')
+st.markdown("Tutkimusappi alueviherkertoimen kehitykseen [Aalto](##)")
 
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+tab1,tab2 = st.tabs(["OpenStreetMap","Testisuunnitelmat"])
+
+with tab1:
+    gdf = None
+    s1,s2 = st.columns(2)
+    add = s1.text_input('Kohdeosoite')
+    tag = s2.radio('',['Land cover','Land-use','Both'],horizontal=True)
+    if tag == 'Land-use':
+        tags = {'landuse':True}
+        name = f"Maankäyttö {add}"
+    elif tag == 'Land cover':
+        tags = {'natural':True}
+        name = f"Maanpeite {add}"
+    else:
+        tags = {'natural':True,'landuse':True}
+        name = f"Maankäyttö ja peite {add}"
+    if add:
+        gdf = utils.get_landuse(add=add,tags=tags,radius=1000)
+        col="type"
+        fig_bar = utils.plot_osm_areas(gdf)
+        st.plotly_chart(fig_bar, use_container_width=True, config = {'displayModeBar': False})
+        
+    if gdf is not None:  
+        fig_osm = utils.plot_landuse(gdf,name=name,col=col)
+        st.plotly_chart(fig_osm, use_container_width=True, config = {'displayModeBar': False})
+        
+with tab2:
+    data = None
+    VE = st.radio("Valitse testisuunnitelman versio",['VE1','VE2'],horizontal=True)
+    if VE == 'VE1':
+        data = conn.read(worksheet="2027374130",ttl="30min")
+    elif VE == 'VE2':
+        data = conn.read(worksheet="1190105635",ttl="30min")
+    else:
+        st.stop()
+        element_list = [f"K{i}" for i in range(1, 30 + 1)]
+        elements = st.multiselect('Valitse testailatavat elementit',element_list,max_selections=5)
+        with st.form('Elementit'):
+            slider_values = {}
+            for e in sorted(elements):
+                slider_values[e] = st.slider(
+                    label=f"{e} Value", 
+                    min_value=0, 
+                    max_value=100, 
+                    value=50,
+                    key=e
+                    )
+            st.text(slider_values)
+            ana = st.form_submit_button('Analysoi')
+            
+    if data is not None:
+        name = "Esimerkkisuunnitelma"
+        df = st.data_editor(data, hide_index=True, height=200, disabled=("wkt"), use_container_width=True)
+        feats = df.drop(columns='wkt').columns.tolist()
+        col = st.selectbox('Visualisoi tieto',feats[1:-1])
+        if col != '..':
+            try: 
+                df['geometry'] = df.wkt.apply(wkt.loads)
+                gdf = gpd.GeoDataFrame(df,geometry='geometry',crs=4326)
+            except:
+                st.stop()
+        
+        if gdf is not None:
+                fig_slu = utils.plot_landuse(gdf,name=name,col=col)
+                st.plotly_chart(fig_slu, use_container_width=True, config = {'displayModeBar': False})
+            
+            
+            
--- app/utils.py
+++ app/utils.py
@@ -1,91 +1,130 @@
 #utils.py
 import streamlit as st
 import pandas as pd
-import boto3
+import geopandas as gpd
+from shapely.geometry import Point, Polygon, box
+import random
+import plotly.express as px
+import osmnx as ox
+ox.config(use_cache=True, log_console=True)
 
-#project bucket keys
-bucket_key = st.secrets["bucket"]['BUCKET_accesskey']
-bucket_secret = st.secrets["bucket"]['BUCKET_secretkey']
-bucket_url = st.secrets["bucket"]['BUCKET_url']
-bucket_name = st.secrets["bucket"]['BUCKET_name']
+px.set_mapbox_access_token(st.secrets['plotly']['MAPBOX_TOKEN'])
+arvo_style = st.secrets['plotly']['MAPBOX_STYLE']
 
 #auth
 def check_password():
-    """Returns `True` if the user had a correct password."""
-
     def password_entered():
-        """Checks whether a password entered by the user is correct."""
         if (
-            st.session_state["username"] in st.secrets["passwords"]
-            and st.session_state["password"]
-            == st.secrets["passwords"][st.session_state["username"]]
+            st.session_state["password"]
+            == st.secrets["passwords"]["cfua"]
         ):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store username + password
-            del st.session_state["username"]
+            del st.session_state["password"]
         else:
             st.session_state["password_correct"] = False
-
     if "password_correct" not in st.session_state:
-        # First run, show inputs for username + password.
-        st.text_input("Username", on_change=password_entered, key="username")
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
+        st.text_input(label="password", type="password", on_change=password_entered, key="password")
         return False
     elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input("Username", on_change=password_entered, key="username")
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 User not known or password incorrect")
+        st.text_input(label="password", type="password", on_change=password_entered, key="password")
         return False
     else:
-        # Password correct.
         return True
-    
 
-#boto3
-def bucket_handler(bucket_name=None, file_name=None, folder_name=None, operation=None, data_frame=None):
-    #session
-    session = boto3.session.Session()
-    client = session.client('s3',
-                            endpoint_url=bucket_url,
-                            aws_access_key_id=bucket_key, 
-                            aws_secret_access_key=bucket_secret 
-                            )
-    
-    if operation == 'download':
-        return download_csv_from_spaces(client, bucket_name, file_name)
-    elif operation == 'upload' and data_frame is not None:
-        upload_csv_to_spaces(client, bucket_name, file_name, data_frame)
-    elif operation == 'list':
-        return list_files_from_bucket(client,bucket_name,folder_name)
+def get_landuse(add,radius,tags = {'natural':True,'landuse':True},removeoverlaps=False):
+    data = ox.features_from_address(add,dist=radius,tags=tags).reset_index()
+    gdf = data.loc[data['geometry'].geom_type.isin(['Polygon', 'MultiPolygon'])]
+    if tags == {'natural':True,'landuse':True}:
+        gdf['type'] = gdf.apply(lambda row: row['landuse'] if pd.notna(row['landuse']) else row['natural'], axis=1)
+    elif tags == {'natural':True}:
+        gdf['type'] = gdf.apply(lambda row: row['natural'], axis=1)
     else:
-        raise ValueError("Invalid operation or missing data for upload")
+        gdf['type'] = gdf.apply(lambda row: row['landuse'], axis=1)
+    #clip
+    loc = ox.geocode(add)
+    center_gdf = gpd.GeoDataFrame(geometry=[Point(loc[1],loc[0])], crs="EPSG:4326")
+    utm = center_gdf.estimate_utm_crs()
+    gdf_utm = gdf.to_crs(utm)
+    gdf_utm['area'] = gdf_utm.area
+    buffer = center_gdf.to_crs(utm).buffer(radius)
+    filtered_gdf = gpd.clip(gdf_utm, buffer)
+    
+    #remove overlaps
+    if removeoverlaps:
+        to_remove = []
+        for index, polygon in filtered_gdf.iterrows():
+            others = filtered_gdf.drop(index)
+            overlaps = others[others.geometry.overlaps(polygon.geometry)]
+            total_overlap_area = sum(overlaps.geometry.intersection(polygon.geometry).area)
+            overlap_percentage = total_overlap_area / polygon.geometry.area
+            if overlap_percentage > 0.01:
+                to_remove.append(index)
+        filtered_gdf = filtered_gdf.drop(to_remove)
+        #recalc area
+        filtered_gdf['area'] = filtered_gdf.area
+        
+    #cols
+    columns=['name','type','area','geometry']
+    return  filtered_gdf.to_crs(4326)[columns]
 
-def download_csv_from_spaces(client, bucket_name, file_name):
-    obj = client.get_object(Bucket=bucket_name, Key=file_name)
-    df = pd.read_csv(obj['Body'])
-    return df
+def plot_landuse(gdf,name,col='type'):
+    lat = gdf.unary_union.centroid.y
+    lon = gdf.unary_union.centroid.x
+    fig_map = px.choropleth_mapbox(gdf,
+                            geojson=gdf.geometry,
+                            locations=gdf.index,
+                            title=name,
+                            color=col,
+                            hover_name=col,
+                            center={"lat": lat, "lon": lon},
+                            mapbox_style=arvo_style,
+                            zoom=13,
+                            opacity=0.5,
+                            width=1200,
+                            height=700
+                            )
 
-def upload_csv_to_spaces(client, bucket_name, file_name, data_frame):
-    csv_buffer = data_frame.to_csv(index=False)
-    client.put_object(Bucket=bucket_name, Key=file_name, Body=csv_buffer, ContentType='text/csv')
+    fig_map.update_layout(margin={"r": 10, "t": 50, "l": 10, "b": 10}, height=700,
+                                legend=dict(
+                                    yanchor="top",
+                                    y=0.97,
+                                    xanchor="left",
+                                    x=0.02
+                                )
+                                )
+    return fig_map
 
-def list_files_from_bucket(client,bucket_name,folder_name):
-    # List objects in the specified folder
-    objects = client.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
+def plot_osm_areas(gdf):
+    fig = px.bar(gdf,x='area',y='type',color='type')
+    fig.update_xaxes(range=[0,gdf['area'].quantile(0.99)])
+    return fig
 
-    # Initialize a list to hold CSV file names
-    csv_files = []
-
-    # Iterate over each object in the specified folder
-    for obj in objects.get('Contents', []):
-        file_name = obj['Key']
-        # Check if the file is a CSV
-        if file_name.endswith('.csv'):
-            csv_files.append(file_name)
-
-    return csv_files
+def create_boxes_from_dict(ks_dict):
+    last_bbox = None
+    last_corner = None
+    data = []  # To hold data for the GeoDataFrame
+    
+    for k, values in ks_dict.items():
+        area, eco_area = values
+        side_length = area**0.5  # Assuming square boxes for simplicity
+        
+        if last_bbox is None:
+            # Arbitrarily place the first box
+            new_bbox = box(60.2826, 24.9384, side_length, side_length)
+        else:
+            # Logic to place the new box relative to the last one, avoiding the last corner used
+            minx, miny, maxx, maxy = last_bbox.bounds
+            corners = [(minx, miny), (minx, maxy), (maxx, miny), (maxx, maxy)]
+            if last_corner in corners:
+                corners.remove(last_corner)
+            new_corner = random.choice(corners)
+            # Example placement logic, places new box to the right of the selected corner
+            new_bbox = box(new_corner[0], new_corner[1], new_corner[0] + side_length, new_corner[1] + side_length)
+            last_corner = new_corner
+        
+        last_bbox = new_bbox
+        data.append([k, new_bbox, area, eco_area])
+    
+    # Create GeoDataFrame
+    gdf = gpd.GeoDataFrame(data, columns=['Key', 'geometry', 'Area', 'Eco_Area'])
+    return gdf
