1dacd7a4302cf623f8ae7a9d027cb813532662d6
Teemu Jama <43031759+teemuja@users.noreply.github.com>
2024-08-14 10:51:10 +0000

    dev1.3+

--- app/location.py
+++ app/location.py
@@ -26,24 +26,27 @@ except:
     st.stop()
     
 with st.expander('Elinympäristöt datassa',expanded=True):
-    #setmin area
+    
+    #reset session_state df when min area changed
     def reset_session_state():
         if "loc_df_updated" in st.session_state:
             del st.session_state.loc_df_updated
-    minarea = st.slider('Min. pinta-ala elinympäristölle (m2)',0,1000,200,100, on_change=reset_session_state)
+            
+    if latlon:
+        minarea = st.slider('Min. pinta-ala elinympäristölle (m2)',0,1000,200,100, on_change=reset_session_state)
     
-    if sorsa == 'OSM':
-        tag = 'Maanpeite' #s3.radio('Lähtötieto',['Maanpeite','Maankäyttö','Luontoalueet'],horizontal=True)
-        if tag == 'Maankäyttö':
-            tags = {'landuse':True}
-            name = f"Maankäyttö {add}"
-        elif tag == 'Luontoalueet':
-            tags = {'natural':True}
-            name = f"Luontoalueet {add}"
-        else:
-            tags = {'natural':True,'landuse':['grass','meadow','forest']}
-            name = f"Elinympäristöt: {add}"
-        if latlon:
+        if sorsa == 'OSM':
+            tag = 'Maanpeite' #s3.radio('Lähtötieto',['Maanpeite','Maankäyttö','Luontoalueet'],horizontal=True)
+            if tag == 'Maankäyttö':
+                tags = {'landuse':True}
+                name = f"Maankäyttö {add}"
+            elif tag == 'Luontoalueet':
+                tags = {'natural':True}
+                name = f"Luontoalueet {add}"
+            else:
+                tags = {'natural':True,'landuse':['grass','meadow','forest']}
+                name = f"Elinympäristöt: {add}"
+            
             try:
                 gdf = utils.get_osm_landuse(latlon=latlon,tags=tags,radius=r)
                 if gdf is not None and not gdf.empty:
@@ -62,11 +65,11 @@ with st.expander('Elinympäristöt datassa',expanded=True):
             except TimeoutError:
                 st.warning('Ei yhteyttä dataan')
                 st.stop()
-                
-    elif sorsa == 'HSY':
-        #with st.expander('HSY avoin data tasot'):
-        #    utils.print_wfs_layers()
-        if latlon:
+                    
+        elif sorsa == 'HSY':
+            #with st.expander('HSY avoin data tasot'):
+            #    utils.print_wfs_layers()
+            
             try:
                 gdf = utils.get_hsy_maanpeite(latlon=latlon,radius=r)
                 if gdf is not None and len(gdf) > 1: 
@@ -93,21 +96,23 @@ with st.expander('Elinympäristöt datassa',expanded=True):
                 st.warning('Ei yhteyttä dataan')
                 st.stop()
 
-    else:
-        gdf_oma = None
-        uploaded_file_oma = st.file_uploader("Lataa oma aineisto", type=['zip'], key='oma')
-        if uploaded_file_oma:
-            try:
-                gdf_oma = utils.extract_shapefiles_from_zip(uploaded_file_oma,'Polygon')
-                if 'area' not in gdf_oma.columns or gdf_oma['area'].isna().all():
-                    utm = gdf_oma.estimate_utm_crs()
-                    gdf_oma['area'] = round(gdf_oma.to_crs(utm).area,-1)
-                else:
-                    pass
-            except Exception as err_bu:
-                print(f"Data error: {err_bu}")
-                st.warning('Tarkista data')
-            st.data_editor(gdf_oma.drop(columns='geometry'))
+        else:
+            gdf_oma = None
+            uploaded_file_oma = st.file_uploader("Lataa oma aineisto", type=['zip'], key='oma')
+            if uploaded_file_oma:
+                try:
+                    gdf_oma = utils.extract_shapefiles_from_zip(uploaded_file_oma,'Polygon')
+                    if 'area' not in gdf_oma.columns or gdf_oma['area'].isna().all():
+                        utm = gdf_oma.estimate_utm_crs()
+                        gdf_oma['area'] = round(gdf_oma.to_crs(utm).area,-1)
+                    else:
+                        pass
+                except Exception as err_bu:
+                    print(f"Data error: {err_bu}")
+                    st.warning('Tarkista data')
+                st.data_editor(gdf_oma.drop(columns='geometry'))
+                st.warning('Oman aineiston analyysi ei vielä käytössä.')
+                st.stop()
 
 
 if gdf is not None and not gdf.empty:
