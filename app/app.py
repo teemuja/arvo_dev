6e65bc5ff6e21122598141c93d30f01bc0915a81
teemuja <teemu.jama@aalto.fi>
2025-11-18 10:43:30 +0000

    V 2025:11

--- app/app.py
+++ app/app.py
@@ -15,32 +15,20 @@ st.set_page_config(page_title="ARVO demot", layout="wide", initial_sidebar_state
 st.sidebar.image('https://figbc.fi/media/arvo_logo.png')
 st.sidebar.header("A R V O demot",divider='green')
 st.sidebar.markdown("T&K-demot alueviherkertoimen kehitykseen [ARVO](https://figbc.fi/arvo-viherrakenteen-arviointi-ja-vahvistaminen-kaupunkien-maankayton-suunnittelussa)-hankkeessa.")
-st.sidebar.caption('Työversio beta 1.4')
-
-with st.container():
-    if "logged_in" not in st.session_state or st.session_state.logged_in == False:
-        st.session_state.logged_in = False
-        with st.empty():
-            utils.check_password()
-        #logos from allas
-        st.image(utils.allas(f"arvodev/appdata/arvologot.png"))
+st.sidebar.caption('V 2025:11')
 
 home_page = st.Page("home.py", title="Miksi demot?", default=True)
-feedback_page = st.Page("feedback.py", title="Kehitysideat")
 location = st.Page("location.py", title="Aineistotestit")
-plans = st.Page("plans.py", title="Suunnitelman analyysi", )
-saved = st.Page("saved.py", title="Tallennetut analyysit", )
-logic = st.Page("logic.py", title="Arviointilogiikan kehitys", )
-#prediction = st.Page("predictor.py", title="Suunnitelman potentiaaliarviointi", )
+poc = st.Page("poc.py", title="Laskentademo", )
 
-if st.session_state.logged_in:
-    menu = st.navigation(
-        {
-            "Info": [home_page],
-            "Demot": [location,logic]
-        }
-    )
-    menu.run()
+menu = st.navigation(
+    {
+        "Info": [home_page],
+        "Demot": [location,poc]
+    }
+)
+
+menu.run()
 
 
 #sidebar footer
--- app/home.py
+++ app/home.py
@@ -1,12 +1,20 @@
 import streamlit as st
-  
-st.subheader('Miksi demot?')
+import utils
+
+st.header("A R V O demot",divider='green')
 
 home_text = """
-    ARVO-hankkeessa kehitetään **_laskentatapaa_**, 
+    ARVO-hankkeessa on kehitetty **_laskentatapaa_**, 
     jolla kaupunkisuunittelun vaikutusta luonnon monimuotoisuuteen ja ekosysteemipalveluihin voidaan arvottaa 
-    sekä maasto- ja paikkatiedosta että suunnitelmista mahdollisimman yhteismitallisesti osana kaavoitusta.  
-    Laskentatapojen testaus sekä eri lähtötietojen toimivuuden tarkastelu laskennan pohjadatana on hankkeessa keskeistä.
-    Demojen avulla testaus **_joukkoistetaan_** sekä hanketiimin että sidosryhmien kesken.
+    sekä maasto- ja paikkatiedosta että suunnitelmista mahdollisimman yhteismitallisesti osana kaavoitusta.
+    Laskentatapojen testaus sekä eri lähtötietojen toimivuuden tarkastelu laskennan pohjadatana oli hankkeessa keskeistä.
+    Demojen avulla testaus **_joukkoistettiin_** sekä hanketiimin että sidosryhmien kesken.
     """
 st.markdown(home_text)
+
+try:
+    img_path = utils.allas(f"arvodev/appdata/arvologot.png")
+    if img_path:
+        st.image(img_path)
+except Exception as e:
+    print("(Logo ei saatavilla)")
